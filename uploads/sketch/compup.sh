03e606492d4308c1ed046235e5198b8532874e94
fbruc03 <65135023+fbruc03@users.noreply.github.com>
2021-03-10 14:02:21 +0100

    Delete compup.sh

--- uploads/sketch/compup.sh
+++
@@ -1,4 +0,0 @@
-echo "compiling and uploading..."
-arduino-cli compile --fqbn sensebox:samd:sb $(pwd)/uploads/sketch
-arduino-cli upload -p /dev/ttyACM0 --fqbn sensebox:samd:sb $(pwd)/uploads/sketch
-echo "done"
