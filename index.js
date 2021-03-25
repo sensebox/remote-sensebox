1800226d59c5c70fd13dfba602c2aea0be7abb23
fbruc03 <65135023+fbruc03@users.noreply.github.com>
2021-03-25 13:03:48 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,30 +5,30 @@ const port = 3000;
 const shell = require('shelljs');
 const fs = require('fs');
 
-// default options
 app.use(fileUpload());
 
 app.get('/upload', (req, res) => res.sendFile(__dirname + "/upload.html"));
 
 app.post('/upload', function(req, res) {
-  let sampleFile;
+  let sketch;
   let uploadPath;
 
   if (!req.files || Object.keys(req.files).length === 0) {
     return res.status(400).send('No files were uploaded.');
   }
 
-  // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
-  sampleFile = req.files.sampleFile;
-  uploadPath = __dirname + '/uploads/sketch/' + sampleFile.name;
+  sketch = req.files.sketch;
+  shell.exec('mkdir ' + __dirname + '/uploads/' + sketch.name.split(".")[0])
+  uploadPath = __dirname + '/uploads/' + sketch.name.split(".")[0] + '/' + sketch.name;
 
-  // Use the mv() method to place the file somewhere on your server
-  sampleFile.mv(uploadPath, function(err) {
+  sketch.mv(uploadPath, function(err) {
     if (err)
       return res.status(500).send(err);
 
-    fs.renameSync(uploadPath , '/home/pi/remote-sensebox/uploads/sketch/sketch.ino');
-    shell.exec('arduino-cli compile --upload ' + __dirname + '/uploads/sketch/sketch.ino --port /dev/ttyACM0 --fqbn sensebox:samd:sb')
+    // compile sketch and flash mcu
+    shell.exec('arduino-cli compile --upload ' + __dirname + '/uploads/' + sketch.name.split(".")[0] + '/' + sketch.name + ' --port /dev/ttyACM0 --fqbn sensebox:samd:sb');
+    // delete folder with sketch
+    shell.exec('rm -r ' + __dirname + '/uploads/' + sketch.name.split(".")[0]);
     res.send('File uploaded!');
   });
 });
