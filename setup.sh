bb7ba96602ee946c8c70ec79dad8743ccf8531e2
fbruc03 <65135023+fbruc03@users.noreply.github.com>
2021-03-29 11:57:44 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,8 +6,8 @@ curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.
 sudo mv bin/arduino-cli /usr/local/bin
 sudo rm -r bin
 
-arduino-cli --additional-urls https://gist.githubusercontent.com/ubergesundheit/112ff5f704e39e3d48c96d691a7031b1/raw/130d3b45a2fffc0a9cb776fadefe03139ae616e2/package_sensebox_index.json core install arduino:samd
-arduino-cli --additional-urls https://gist.githubusercontent.com/ubergesundheit/112ff5f704e39e3d48c96d691a7031b1/raw/130d3b45a2fffc0a9cb776fadefe03139ae616e2/package_sensebox_index.json core install sensebox:samd
+arduino-cli --additional-urls https://raw.githubusercontent.com/sensebox/senseBoxMCU-core/master/package_sensebox_index.json core install arduino:samd
+arduino-cli --additional-urls https://raw.githubusercontent.com/sensebox/senseBoxMCU-core/master/package_sensebox_index.json core install sensebox:samd
 
 git clone https://github.com/arduino-libraries/SD /home/pi/.arduino15/packages/sensebox/hardware/samd/1.3.3/libraries/SD
 
