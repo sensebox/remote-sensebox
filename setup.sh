7f291408b32caeed9562ddca0e08e10cc358d50a
fbruc03 <65135023+fbruc03@users.noreply.github.com>
2021-03-29 12:18:36 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ sudo rm -r bin
 arduino-cli --additional-urls https://raw.githubusercontent.com/sensebox/senseBoxMCU-core/master/package_sensebox_index.json core install arduino:samd
 arduino-cli --additional-urls https://raw.githubusercontent.com/sensebox/senseBoxMCU-core/master/package_sensebox_index.json core install sensebox:samd
 
-git clone https://github.com/arduino-libraries/SD /home/pi/.arduino15/packages/sensebox/hardware/samd/1.3.3/libraries/SD
+git clone https://github.com/arduino-libraries/SD /home/pi/.arduino15/packages/sensebox/hardware/samd/1.4.0/libraries/SD
 
 sudo apt-get install nodejs -y
 
